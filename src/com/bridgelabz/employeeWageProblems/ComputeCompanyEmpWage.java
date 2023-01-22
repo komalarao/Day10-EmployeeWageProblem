c8bfaf88376f10625ede92df68fddc75a2fd34ca
komalarao <manoharvakamullu1999@gmail.com>
2023-01-22 10:17:17 -0800

    Refactor: get total wage queried by company

--- src/com/bridgelabz/employeeWageProblems/ComputeCompanyEmpWage.java
+++ src/com/bridgelabz/employeeWageProblems/ComputeCompanyEmpWage.java
@@ -3,4 +3,6 @@ package com.bridgelabz.employeeWageProblems;
 public interface ComputeCompanyEmpWage {
 	public void addCompany(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth);
     public void computeEmpWage();
+    public int getTotalEmpWage(String company);
+
 }
